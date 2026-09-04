6fccc45058a0d85e8695f222faf3af24be2cfab0
𝐀𝐛𝐝𝐮𝐥 𝐑𝐚𝐮𝐟 <raufrana0322@gmail.com>
2026-09-04 11:30:56 +0500

    Refactor index.js to include dotenv and clean up code

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -2,7 +2,7 @@ const express =require('express')
 const  cors=require('cors')
 const mongoose = require('mongoose')
 const ConnectDB = require('./service')
-
+require('dotenv').config()
 
 ConnectDB()
 
